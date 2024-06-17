e06ca25da059175d2ecf790fb1ad02abe2ce696a
YourUsername <you@example.com>
2024-06-17 13:47:38 +0300

    auto api url selecting

--- src/config.jsx
+++ src/config.jsx
@@ -1,2 +1,4 @@
-export const API_URL = window.location.hostname ? "http://127.0.0.1:8000/" : "https://greatmartyr-japanese-story-generator.hf.space/"
+export const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+    ? "http://127.0.0.1:8000/" : "https://greatmartyr-japanese-story-generator.hf.space/"
+    
 export const API_TIMEOUT = 20000
