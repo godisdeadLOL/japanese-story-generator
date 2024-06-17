020037abc55c208c11b5b3d2eecaf44294752e73
YourUsername <you@example.com>
2024-06-17 12:58:50 +0300

    fixed invalid_type error

--- backend/core/config.py
+++ backend/core/config.py
@@ -9,7 +9,7 @@ openai_url = os.environ['OPENAI_URL']
 openai_key = os.environ['OPENAI_KEY']
 openai_timeout = int(os.environ['OPENAI_TIMEOUT'])
 openai_model = os.environ['OPENAI_MODEL']
-openai_max_tokens = os.environ['OPENAI_MAX_TOKENS']
+openai_max_tokens = int(os.environ['OPENAI_MAX_TOKENS'])
 
 allow_origins = os.environ['ALLOW_ORIGINS'].split(',')
 
